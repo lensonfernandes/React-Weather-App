8ac0fe4eb67172585eb8adcb537c22e9a0e5d4a2
lensonfernandes <lenson.fernand@gmail.com>
2022-11-06 18:47:06 +0530

    Added Forecast Min and Max temperature

---
+++ reactweatherapp/src/api.js
@@ -0,0 +1,16 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4d51bdb949mshf86800867c9df4bp1e2348jsn4d7d96b905ee',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+// export const WEATHER_API_URL = "https://api.openweathermap.org/data/3.0";
+
+// export const weather_API_KEY = "e5045478fc983d383efddcb2bf93cfd0";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e5045478fc983d383efddcb2bf93cfd0"; // enter your key from openweather API
